d0d4baa799d40d03bd3f6cefc929621274d553f0
Hung-Hsuan Chen <hhchen@psu.edu>
2013-08-06 15:51:18 -0400

    Create the database if it doen't exist

--- src/python/gen_keyphrase/mysql_util.py
+++ src/python/gen_keyphrase/mysql_util.py
@@ -6,7 +6,7 @@ def init_db():
   db = MySQLdb.connect(host=db_info['host'], user=db_info['user'], \
       passwd=db_info['passwd'], unix_socket=db_info['socket'])
   cursor = db.cursor()
-  if not is_db_exists(db, cursor, db_info['db']):
+  if not does_db_exist(db, cursor, db_info['db']):
     cursor.execute("CREATE DATABASE " + db_info['db'] + " DEFAULT CHARACTER SET utf8")
   db.select_db(db_info['db'])
 
@@ -43,13 +43,13 @@ def get_db_info():
   f.close()
   return db_info
 
-def is_db_exists(db, cursor, db_name):
+def does_db_exist(db, cursor, db_name):
   cursor.execute("""SELECT schema_name FROM information_schema.schemata """ + \
      """WHERE schema_name = %s""", (db_name))
   row = cursor.fetchone()
   return row != None
 
-def is_tbl_exists(db, cursor, table_name):
+def does_table_exist(db, cursor, table_name):
   db_info = get_db_info()
   cursor.execute("""SELECT * FROM information_schema.tables """ + \
       """WHERE table_schema = %s AND table_name = %s""", (db_info['db'], table_name))
@@ -57,7 +57,29 @@ def is_tbl_exists(db, cursor, table_name):
   return row != None
 
 def drop_tbl(db, cursor, table_name):
-  if is_tbl_exists(db, cursor, table_name):
+  if does_table_exist(db, cursor, table_name):
     cursor.execute("""DROP TABLE """ + table_name)
 
+def does_col_exist(table_name, column_name):
+  db_info = get_db_info()
+  db, cursor = init_db()
+  cursor.execute("""SELECT * FROM information_schema.columns """ + \
+      """WHERE table_schema = %s AND table_name = %s AND column_name LIKE %s""", \
+      (db_info['db'], table_name, column_name))
+  row = cursor.fetchone()
+  close_db(db, cursor)
+  return row != None
+
+def add_index(tbl_name, col_name, idx_type):
+  if idx_type.lower() not in ['index', 'unique index', 'fulltext']:
+    raise Exception('index type can only be "index" or "unique index"')
+
+  if not does_table_exist(tbl_name):
+    raise Exception("Table " + tbl_name + " does not exist in the database")
+  if not does_col_exist(tbl_name, col_name):
+    raise Exception("Column " + col_name + " does not exist in table " + tbl_name)
+
+  db, cursor = init_db()
+  cursor.execute('ALTER TABLE ' + tbl_name + ' ADD ' + idx_type + '(' + col_name + ')')
+  close_db(db, cursor)
 
